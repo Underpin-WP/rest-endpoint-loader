9eb0af64783a146b43f23f16f06be41eb1c51543
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:16:41 -0700

    Corrects underpin autoloader

--- rest-endpoints.php
+++ rest-endpoints.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Rest_Endpoint.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Rest_Endpoint_Instance.php' );
-	Underpin\underpin()->get( $file )->loaders()->add( 'rest_endpoints', [
+	Underpin\underpin()->get( $file, $class )->loaders()->add( 'rest_endpoints', [
 		'instance' => 'Underpin_Rest_Endpoints\Abstracts\Rest_Endpoint',
 		'default'  => 'Underpin_Rest_Endpoints\Factories\Rest_Endpoint_Instance',
 	] );
-} );
+}, 10, 2 );
